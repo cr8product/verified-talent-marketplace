d44a110356c2b7f7918f47b472c02edcee8d482a
Metin <aknmetin@gmail.com>
2023-05-13 21:25:50 +0000

    Tried to implement PolygonID

---
+++ src/components/Verify.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { useContext } from 'react';
+import TalentLayerContext from '../context/talentLayer';
+import useUserById from '../hooks/useUserById';
+import PohModule from '../modules/Poh/PohModule';
+import { IUser } from '../types';
+import Loading from './Loading';
+import Stars from './Stars';
+import Image from 'next/image';
+
+function Verify() {
+
+  return (
+    <div className='flex flex-col rounded-xl p-4 border border-gray-200'>
+      <div className='flex items-top justify-between w-full'>
+        <div className='flex flex-col justify-start items-start gap-4'>
+          <div className='flex items-center justify-start mb-4'>
+            <h1> Hello World</h1>
+          </div>
+        </div>
+      </div>
+    </div>            
+
+export default Verify;
