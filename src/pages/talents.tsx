53ccc18bb7054a1fe89c9f642848476974da897e
Metin <aknmetin@gmail.com>
2023-05-13 21:27:56 +0000

    PolygonID initial implementation

--- src/pages/talents.tsx
+++ src/pages/talents.tsx
@@ -3,67 +3,58 @@ import SearchTalentButton from '../components/Form/SearchTalentButton';
 import Loading from '../components/Loading';
 import UserItem from '../components/UserItem';
 import useUsers from '../hooks/useUsers';
+import { QRCodeSVG } from "qrcode.react";
+
 
 function Talents() {
+  
+  //Create a map to store the auth requests and their session IDs
+  const requestMap = new Map();
+
+  
   const PAGE_SIZE = 36;
   const router = useRouter();
   const query = router.query;
   const searchQuery = query.search as string;
+  const deployedContractAddress = 0xecf178144ccec09417412d66e2ecc8a2841ee228;
   const { users, hasMoreData, loading, loadMore } = useUsers(
     searchQuery?.toLocaleLowerCase(),
     PAGE_SIZE,
   );
+  const qrProofRequestJson = {
+    "@context": [{
+        "@version": 1.1,
+        "@protected": true,
+        "id": "@id",
+        "type": "@type",
+        "Lisbon1Age": {
+            "@id": "https://s3.eu-west-1.amazonaws.com/polygonid-schemas/5788c821-469b-48d1-9bf8-bab5bbea8abd.json-ld#Lisbon1Age",
+            "@context" : {
+                "@version": 1.1,
+                "@protected": true,
+                "id": "@id",
+                "type": "@type",
+                "poly-vocab": "https://s3.eu-west-1.amazonaws.com/polygonid-schemas/5788c821-469b-48d1-9bf8-bab5bbea8abd.vocab.schema.json",
+                "serialization": "https://github.com/iden3/claim-schema-vocab/blob/main/credentials/serialization.md#",
+                "What is your age?": {
+                    "@id": "poly-vocab:What is your age?",
+                    "@type": "serialization:IndexDataSlotA"
+                }
+            }
+        }
+    }]
+  };
 
   return (
     <div className='max-w-7xl mx-auto text-gray-900 sm:px-4 lg:px-0'>
       <p className='text-5xl font-medium tracking-wider mb-8'>
-        All <span className='text-indigo-600'>Talents </span>
+        Start to <span className='text-indigo-600'>Verify Yourself </span>
       </p>
 
-      {searchQuery && users.length > 0 && (
-        <p className='text-xl font-medium tracking-wider mb-8'>
-          Search results for <span className='text-indigo-600'>{searchQuery}</span>
-        </p>
-      )}
-      {searchQuery && users.length === 0 && (
-        <p className='text-xl font-medium tracking-wider mb-8'>
-          No search results for <span className='text-indigo-600'>{searchQuery}</span>
-        </p>
-      )}
-
-      <div className='flex justify-center items-center gap-10 flex-col pb-5'>
-        <SearchTalentButton value={searchQuery} />
+      <div className='flex justify-center items-centers'>
+        <QRCodeSVG level="Q" value={JSON.stringify(qrProofRequestJson)} />
       </div>
 
-      <div className='grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
-        {users.map((user, i) => {
-          return <UserItem user={user} key={i} />;
-        })}
-      </div>
-
-      {users.length > 0 && hasMoreData && !loading && (
-        <div className='flex justify-center items-center gap-10 flex-col pb-5'>
-          <button
-            type='submit'
-            className={`px-5 py-2 mt-5 content-center border border-indigo-600 rounded-full text-indigo-600 
-              hover:text-white hover:bg-indigo-700
-            `}
-            disabled={!hasMoreData}
-            onClick={() => loadMore()}>
-            Load More
-          </button>
-        </div>
-      )}
-      {loading && (
-        <div className='flex justify-center items-center gap-10 flex-col pb-5 mt-5'>
-          <Loading />
-        </div>
-      )}
-      {!hasMoreData && (
-        <div className='flex justify-center items-center gap-10 flex-col pb-5 mt-5'>
-          <p>No more Users...</p>
-        </div>
-      )}
     </div>
   );
 }
